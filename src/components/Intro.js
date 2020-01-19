1aa607ca00d25b44cfdc6e41fb8d4cf7a666fa61
Ryan <parag.ryan@gmail.com>
2020-01-19 17:35:42 -0500

    change filter

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Badge } from './Badge';
-import { H1,H2,H3,H4,H5,H6 } from './text';
+import { H2,H5 } from './text';
 import Logo from './Logo';
 import { ContainerItemSmall } from './ContainerItem';
 
--- src/components/ThemeList.js
+++ src/components/ThemeList.js
@@ -5,29 +5,27 @@ import SearchInput from './SearchInput';
 import { ContainerItemLarge } from './ContainerItem';
 import themes from '../data/themes';
 
-class ThemeList extends Component {
-  render() {
-    return (
-      <ContainerItemLarge>
-        <SearchInput />
-        <ThemeGrid>
-          {themes.map(theme => (
-            <SlackWidget 
-              title={theme.name}
-              columnBg={theme.colors.columnBg}
-              menuBgHover={theme.colors.menuBgHover}
-              activeItem={theme.colors.activeItem}
-              activeItemText={theme.colors.activeItemText}
-              hoverItem={theme.colors.hoverItem}
-              textColor={theme.colors.textColor}
-              activePresence={theme.colors.activePresence}
-              mentionBadge={theme.colors.mentionBadge}
-            />
-          ))}
-        </ThemeGrid>
-      </ContainerItemLarge>
-    )
-  }
+const ThemeList = () => {
+  return (
+    <ContainerItemLarge>
+      <SearchInput />
+      <ThemeGrid>
+      {themes.map(theme => (
+        <SlackWidget 
+          title={theme.name}
+          columnBg={theme.colors.columnBg}
+          menuBgHover={theme.colors.menuBgHover}
+          activeItem={theme.colors.activeItem}
+          activeItemText={theme.colors.activeItemText}
+          hoverItem={theme.colors.hoverItem}
+          textColor={theme.colors.textColor}
+          activePresence={theme.colors.activePresence}
+          mentionBadge={theme.colors.mentionBadge}
+        />
+      ))}
+      </ThemeGrid>
+    </ContainerItemLarge>
+  )
 }
 
 export default ThemeList;
