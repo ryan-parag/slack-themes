0573c8fe39adc2f91befa44ca36cba0330be4747
Ryan Parag <parag.ryan@gmail.com>
2020-04-04 17:08:05 -0400

    fix hotloader issue and add contribution link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,13 +9,24 @@ const FooterContainer = styled.footer`
   padding: 1.6rem 1.6rem 3.2rem;
 `;
 
+const FooterLink = styled.a`
+  display: inline-flex;
+  padding: 0.8rem 1.6rem;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+  color: ${({theme}) => theme.rootColor};
+  border: 1px solid;
+  border-radius: 0.4rem;
+`;
+
 const Footer = () => {
   return (
     <FooterContainer>
       <p>
         Designed and Developed by <a href="https://grapalab.com" target="_blank">Ryan Parag</a> and <a href="https://matt-broughton.com" target="_blank">Matthew Broughton</a>
       </p>
-      <Button>Contribute on GitHub</Button>
+      <FooterLink href="https://github.com/ryan-parag/slack-themes" target="_blank">Contribute on GitHub</FooterLink>
     </FooterContainer>
   )
 }
