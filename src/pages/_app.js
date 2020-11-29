6156d06e51d381d64196f0fd9006dc9d813c34a9
Ryan Parag <parag.ryan@gmail.com>
2020-11-29 16:41:49 -0500

    fix tailwind styles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import '../styles/tailwind.css';
+import '../styles/tailwind.css'
+import '../styles/index.css'
+import '../styles/tailwind-utils.css'
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
