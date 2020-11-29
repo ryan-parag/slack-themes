30840eec3f304c1a0b5a97450f94160847192192
Ryan Parag <parag.ryan@gmail.com>
2020-11-29 16:16:13 -0500

    data urls

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
+  apiKey: 'AIzaSyDt1Ge9NRBJ36xlutw3AxUc5I-N6PQa6e8',
+  authDomain: 'slack-themes.firebaseapp.com',
+  databaseURL: 'https://slack-themes.firebaseio.com',
+  projectId: 'slack-themes',
+  storageBucket: 'slack-themes.appspot.com',
+  messagingSenderId: '979998890276',
+  appId: '1:979998890276:web:4d29051641af07efd73bb1'
 };
 
 
