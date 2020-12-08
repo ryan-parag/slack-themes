24f12f55d923d57ad96930363f7f5d949b3d5a87
Ryan Parag <parag.ryan@gmail.com>
2020-12-07 21:39:20 -0500

    update vercel

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDt1Ge9NRBJ36xlutw3AxUc5I-N6PQa6e8',
-  authDomain: 'slack-themes.firebaseapp.com',
-  databaseURL: 'https://slack-themes.firebaseio.com',
-  projectId: 'slack-themes',
-  storageBucket: 'slack-themes.appspot.com',
-  messagingSenderId: '979998890276',
-  appId: '1:979998890276:web:4d29051641af07efd73bb1'
+  apiKey: process.env.DB_API_TOKEN,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 };
 
 export default !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
