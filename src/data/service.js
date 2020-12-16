f2706907ae80b96aea216c54409af35a12151048
Ryan Parag <parag.ryan@gmail.com>
2020-12-15 23:19:23 -0500

    test auth tokens

--- src/data/service.js
+++ src/data/service.js
@@ -1,4 +1,4 @@
-module.exports = {
+/*module.exports = {
   type: process.env.SERVICE_TYPE,
   project_id: process.env.SERVICE_PROJECT_ID,
   private_key_id: process.env.SERVICE_PRIVATE_KEY_ID,
@@ -9,4 +9,17 @@ module.exports = {
   token_uri: process.env.SERVICE_TOKEN_URI,
   auth_provider_x509_cert_url: process.env.SERVICE_AUTH_PROVIDER_X509_CERT_URL,
   client_x509_cert_url: process.env.SERVICE_CLIENT_X509_CERT_URL
+}*/
+
+module.exports = {
+  type: "service_account",
+  project_id: "slack-themes",
+  private_key_id: "1198a7bafe63ecb4e05a2771377a9684b7cb4f3a",
+  private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDML/7qRcgkHgXl\ni0S1A40WSFu6TPwG6NbgkP5y8xeshdwXAdktKt/JvyyylMnFxH8fvZ0vQhokuR5M\nCzkVeGhUhtvkEl3/7FVUPLSYaIKrPYEBetLh3RJc8vcTWTDuwdlMLFBvLn0aOanD\noJbpoDnwSCR4oQf2HkIELfcPpTqWFM+WsQfoqpIEkaLe7/vXSz8wzFukdj98ZqQq\nZwF7CC1b3uDxScD+/08fRQJvTix7lEjL+mHLkMHEeqoNpD0vjuiIVpRgl4syhhRO\nVNK9ftbUBsRWFOgPVbeXTlJulhsMjlXDOZO4oy1C8HNqSLM41PvqLv4z86PRY8C6\nouLGCWsrAgMBAAECggEAVC45b0ZjtBg4oCEhq2sSYKxmPNF+Wi7mX+EHNkbobLUn\nPskBS3DQoHFTsTBMW1+edWyMAC7FbnxvdDRwTVy9zpNm1omH/Xnm1/rLDcaQkJ6j\nsAN+K6Yj6vQ0TuLHnLeIqbHfW2cUJpq9VazAawMmv9xORPlGuf5Rgz2cDPQ2yiZ/\nt47lOvwSj48SPoiVOaRwso7r7qU0eUV1mK8Ch6NC6701gxDyaO4N8Wp8riVp77WI\nLS3mh47ViyDiKtbpeNDneAvAKN7W9xBIuDMKcswHTVhBgRNhdi0RLhXuSy3/9YWl\nHD2Q4tYFk4GFdW18BwAveJFyILailUX/hiFs2AY3yQKBgQDqWflAI3r8QG6Fx2fV\nTZz5jSaXAc/lp0Pe0ngkLauynv+3ZYD9hWYvwt//Q6UWLy1SQHNyRkRZTY5kEZRw\neGOu+rMLXaQ4tPuozrXHM1SE0mvN4R4sF76Y/6xX4r81E4Y5q9atiATRc50U9lf1\nXvPT9UVPPifo+HVnumRlEa1R1QKBgQDfDLGYZbb8AyrH1166m6rINjBJBYSoR43n\nYpiQ2m8JCj3odf9OOa+lNQg5gKrqOtwhNBB5i6I4clBrWxy/sLGpBmI+CAxFXmKZ\nbYbSQatDG7tIYek6QG172flteYWX+J5YirA7PdPNWd2POGHbatoEXyLAzLmJEUz5\ne/AzbV1I/wKBgQCOgzSOCoWJzOzpMcA0+MPTBkFHZ3Sz5aniYP9qkNNvBj3fmzx7\n75maxdlN9Cb6fKoE8OjDv3NlE0LaG863Lp8AE7i52rI5QeYgW8QzZUWqmAOy6Yld\nqvrfM+vsbhl0/3CKtbMRSDwV20bVLpGCx8FjTi5d6CCBDkbnU3Ql+j/6GQKBgEt9\nu1nuCxmOZNtQTFJyP0IV/Q+p23UtCp0mpKZaF7yCh+N2lFtAcO9EXK1BnBc8pqZi\nPbU1ikj6f7dmFfvW7LpdW8DM7oJH6K0vAbOF4FYNO7zKzqoVhjil1KK1v35o+gbT\nocPbMCVPZgqENbKSnolePZ/talo2zcueo/Rz7l+3AoGAD0lod7BAl+joPec5TRYt\nAQiVbKpcQCGpDNmPhn9CDN3idaY9VQW4GpbB0NmGMGsOfMfyro5/E2ZRcvawUvd4\n59b2Mvw41Eb41cHRWIhfz70U9d2nGPG8tZErGT9ATvF7iKeZc1u4a/1zZYbWZD4e\nAxOmM+oQVaSnVuRuQxeNAII=\n-----END PRIVATE KEY-----\n",
+  client_email: "firebase-adminsdk-vtrbp@slack-themes.iam.gserviceaccount.com",
+  client_id: "102480317728817322902",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-vtrbp%40slack-themes.iam.gserviceaccount.com"
 }
