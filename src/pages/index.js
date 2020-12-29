77b2718fdbf6450d7426548cc0de12a4c0834af8
Ryan Parag <parag.ryan@gmail.com>
2020-12-28 22:48:42 -0500

    remove query params

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,16 +10,16 @@ import { motion } from 'framer-motion'
 import { Loader, Search , Sliders} from 'react-feather'
 import { useRouter } from 'next/router'
 
-export default function Home(props) {
+export default function Home() {
 
   const [filteredThemes, setFilteredThemes] = useState([])
   const [loading, setLoading] = useState(true)
-  const [query, setQuery] = useState(props.filter ? props.filter : '')
-  const [sort, setSort] = useState(props.sort ? props.sort : 'theme_name')
-  const [order, setOrder] = useState(props.order ? props.order : 'asc')
-  //const [query, setQuery] = useState('')
-  //const [sort, setSort] = useState('theme_name')
-  //const [order, setOrder] = useState('asc')
+  //const [query, setQuery] = useState(props.filter ? props.filter : '')
+  //const [sort, setSort] = useState(props.sort ? props.sort : 'theme_name')
+  //const [order, setOrder] = useState(props.order ? props.order : 'asc')
+  const [query, setQuery] = useState('')
+  const [sort, setSort] = useState('theme_name')
+  const [order, setOrder] = useState('asc')
   const [queryAmount, setQueryAmount] = useState(27)
   const [dataSize, setDataSize] = useState(null)
 
@@ -60,7 +60,7 @@ export default function Home(props) {
 
   useEffect(() => {
 
-    router.push(`/?order=${order}&sort=${sort}${query !== '' ? `&filter=${query}` : ''}`, undefined, { shallow: true })
+    //router.push(`/?order=${order}&sort=${sort}${query !== '' ? `&filter=${query}` : ''}`, undefined, { shallow: true })
 
     setLoading(true)
       if(query !== '') {
@@ -211,14 +211,3 @@ export default function Home(props) {
     </Layout>
   );
 }
-
-Home.getInitialProps = async ({query}) => {
-
-  const { sort, filter, order } = query
-
-  return {
-    sort,
-    filter,
-    order
-  };
-};
